d9fcca8a7eb05bba942a7bcdb85d41848712e898
Ezra <ezra.farrar@gmail.com>
2021-03-24 14:53:52 -0700

    Added campaigns view

--- ArcaneLogbook/ArcaneLogbook.Web/Controllers/HomeController.cs
+++ ArcaneLogbook/ArcaneLogbook.Web/Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace ArcaneLogbook.Web.Controllers
             return View(model);
         }
 
+        public ActionResult Campaigns()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
