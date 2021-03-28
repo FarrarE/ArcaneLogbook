64565a32c1eda92fde4eb63cae7fe232ea5da109
Ezra Farrar <ezra.farrar@gmail.com>
2021-03-27 21:39:38 -0700

    campaigns uuid generation for list

--- ArcaneLogbook/ArcaneLogbook.Web/src/Components/Card/index.js
+++ ArcaneLogbook/ArcaneLogbook.Web/src/Components/Card/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Card(props) {
     return (
-        <div className="card">
+        <div className="card" onClick={() => props.onClick(props.id)}>
             <h3>{props.title}</h3>
             <div>
                 {props.body}
--- ArcaneLogbook/ArcaneLogbook.Web/src/Containers/Campaigns/Campaigns.js
+++ ArcaneLogbook/ArcaneLogbook.Web/src/Containers/Campaigns/Campaigns.js
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from '../../Components/Card';
-
+import { v4 as uuid } from 'uuid';
 function Campaigns(props) {
+    const [isLoading, setIsLoading] = useState(false);
+    const [campaignList, setCampaignList] = useState([]);
+
+    useEffect(()=>{
+        console.log("Rerender")
+    },[]);
+
+    function goTo(id){
+        location.assign(location.href + "/campaign/" + id)
+    }
+
+    function addCampaign(){
+        var temp = <Card title="CandleKeep Mysteries" body="Info and stuff" id={uuid()} onClick={goTo} key={campaignList.length}/>;
+        setCampaignList([...campaignList, temp]);
+    }
+
+
+
     return (
-        <div className="container view">
-            <div id="campaign-header">
-                <h2>Campaigns</h2>
-                <button className="add-campaign">Add</button>
+        isLoading ?
+            <div className="container view">
+                <div id="campaign-header">
+                    <h2>Campaigns</h2>
+                    <button className="add-campaign" onClick={addCampaign} >Add</button>
+                </div>
             </div>
-            <Card title="CandleKeep Mysteries" body="Info and stuff" />
-            <Card title="CandleKeep Mysteries" body="Info and stuff" />
-            <Card title="CandleKeep Mysteries" body="Info and stuff" />
-            <Card title="CandleKeep Mysteries" body="Info and stuff" />
-            <Card title="CandleKeep Mysteries" body="Info and stuff" />
-            <Card title="CandleKeep Mysteries" body="Info and stuff" />
-            <Card title="CandleKeep Mysteries" body="Info and stuff" />
-            <Card title="CandleKeep Mysteries" body="Info and stuff" />
-        </div>
+            :
+            <div className="container view">
+                <div id="campaign-header">
+                    <h2>Campaigns</h2>
+                    <button className="add-campaign" onClick={addCampaign} >Add</button>
+                </div>
+                {campaignList}
+            </div>
     )
 }
 
