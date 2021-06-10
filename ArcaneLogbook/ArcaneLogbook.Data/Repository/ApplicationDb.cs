137d0ce94b1da40f10a20fb1b215e4486ef00a18
Ezra Farrar <ezra.farrar@gmail.com>
2021-06-10 12:44:43 -0700

    Added base files for DB

---
+++ ArcaneLogbook/ArcaneLogbook.Data/Repository/ApplicationDb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcaneLogbook.Data.Repository
+{
+    class ApplicationDb
+    {
+        private readonly string _connectionString;
+
+        private ApplicationDb(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public static ApplicationDb Create(string connectionString)
+        {
+            return new ApplicationDb(connectionString);
+        }
+    }
+}
---
+++ ArcaneLogbook/ArcaneLogbook.Data/Services/ApplicationService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcaneLogbook.Data.Repository;
+
+namespace ArcaneLogbook.Data.Services
+{
+    class ApplicationService
+    {
+        private readonly ApplicationDb _applicationDb;
+
+        private ApplicationService(ApplicationDb applicationDb)
+        {
+            _applicationDb = applicationDb;
+        }
+
+        public static ApplicationService Create(ApplicationDb application)
+        {
+            return new ApplicationService(application);
+        }
+
+
+    }
+}
