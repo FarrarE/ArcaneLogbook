40eb1656b449768b8c6f6e653840639f13831a8c
Ezra Farrar <ezra.farrar@gmail.com>
2021-06-14 10:01:12 -0700

    Added View/Controller for characters

---
+++ ArcaneLogbook/ArcaneLogbook.Web/Controllers/CharactersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArcaneLogbook.Web.Controllers
+{
+    public class CharactersController : Controller
+    {
+        // GET: Characters
+        public ActionResult Index()
+        {
+            return View("PlayerCharacters");
+        }
+
+        public ActionResult NonPlayerCharacters()
+        {
+            return View();
+        }
+    }
+}
